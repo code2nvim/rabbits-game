36bac0662a86ac0a093452dd8679f5930b9a9552
code2nvim <code2nvim@gmail.com>
2024-01-20 23:36:04 +0800

    refactoring: arrow functions

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -13,57 +13,62 @@ export default function Game() {
   const [playing, setPlaying] = useState(false);
   const [targets, setTargets] = useState<Target[]>(new Array(9).fill(hole));
 
-  function startGame() {
+  const startGame = () => {
     setScore(0);
     setPlaying(true);
     setTargets(new Array(9).fill(hole));
-  }
+  };
 
-  function endGame(reason: Target) {
+  const endGame = (reason: Target) => {
     setPlaying(false);
     setTargets(new Array(9).fill(hole));
     if (reason === carrot) setMsg("Carrot's gone!");
     if (reason === rabbit) setMsg("It's an innocent rabbit!");
-  }
+  };
 
-  function changeTarget(idx: number, target: Target) {
-    setTargets((pre) => {
-      const newTargets = [...pre];
+  const changeTarget = (idx: number, target: Target) => {
+    // const newTargets = [...targets];
+    // newTargets[idx] = target;
+    // setTargets(newTargets);
+
+    // callback
+    setTargets((preTarget) => {
+      const newTargets = [...preTarget];
       newTargets[idx] = target;
       return newTargets;
     });
-  }
+  };
 
-  function rabbitUp(idx: number) {
+  const rabbitUp = (idx: number) => {
     if (targets[idx] === hole) {
       const rand = Math.floor(Math.random() * 2);
       const target = rand === 0 ? rabbit : carrot;
       changeTarget(idx, target);
     }
-  }
+  };
 
-  function rabbitDown(idx: number) {
+  const rabbitDown = (idx: number) => {
     if (targets[idx] === carrot) {
       endGame(carrot);
     } else {
       changeTarget(idx, hole);
     }
-  }
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (playing) {
+    if (playing) {
+      const interval = setInterval(() => {
         const idx = Math.floor(Math.random() * targets.length);
         rabbitUp(idx);
         setTimeout(() => {
           rabbitDown(idx);
         }, 1500);
-      }
-    }, 200);
-    return () => clearInterval(interval);
+      }, 200);
+      return () => clearInterval(interval);
+    }
   });
 
-  function clickTarget(idx: number) {
+  const clickTarget = (idx: number) => {
     if (playing) {
       switch (targets[idx]) {
         case hole:
@@ -79,9 +84,9 @@ export default function Game() {
           throw new Error("Unknown action");
       }
     }
-  }
+  };
 
-  function assets(target: Target): string {
+  const assets = (target: Target): string => {
     switch (target) {
       case carrot:
         return require("./assets/carrot.png");
@@ -92,7 +97,7 @@ export default function Game() {
       default:
         throw new Error("Unknown action");
     }
-  }
+  };
 
   return (
     <>
@@ -112,6 +117,8 @@ export default function Game() {
       </div>
       <p className="status">
         <span className="msg">{!playing && msg}</span>
+        <br />
+        <span className="score">Fixing bugs...</span>
         <br />
         <span className="score">SCORE: {score}</span>
         <br />
